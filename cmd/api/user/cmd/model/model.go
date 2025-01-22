95ce2bdd3350a3b3648d0b3d79dff81b6f5600b6
hyeonwoody <hyeonwoody@gmail.com>
2025-01-22 14:27:58 +0900

    🛢️DB : Add created_at field in user

--- cmd/api/user/cmd/model/model.go
+++ cmd/api/user/cmd/model/model.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -13,6 +15,7 @@ type Model struct {
 	SecretAccessKey string `gorm:"not null"`
 	SecurityGroupId string `gorm:"not null"`
 	Ec2InstanceId   string
+	CreatedAt       time.Time `gorm:"autoCreateTime"`
 }
 
 func (Model) TableName() string {
